ee3fca1f5f7fd39cd22cfe807bbd9c646c3756d1
Marta <martabarrachina1@hotmail.com>
2017-10-27 12:30:03 +0200

    Aparece el menú, pero no hace nada

--- app/src/main/java/edu/upc/eseiaat/pma/shoppinglist/ShoppingListActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/shoppinglist/ShoppingListActivity.java
@@ -7,6 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -187,6 +189,10 @@ public class ShoppingListActivity extends AppCompatActivity {
         }
     }
 
-
-
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) { // objecto para rellenar
+        MenuInflater inflater = getMenuInflater(); // inflador de menús
+        inflater.inflate(R.menu.options,menu); //  ar partir de ése recurso, mételo en menu
+        return true;
+    }
 }
